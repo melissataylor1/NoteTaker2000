6a215d9f0a15b8d87278bc8f78a8787873beda13
m1taylor <m1taylor@ryerson.ca>
2023-02-16 17:30:46 -0500

    added first dependencies to server.js file

---
+++ server.js
@@ -0,0 +1,7 @@
+//dependencies
+const express = require("express");
+const path = require("path");
+const api = require("./lib/index.js");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
