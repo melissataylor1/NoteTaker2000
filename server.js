b76da3b2f88aaeabb96fb62bdd3042f607963a7a
m1taylor <m1taylor@ryerson.ca>
2023-02-16 17:35:41 -0500

    added middleware and get routes for html pahges

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ const api = require("./lib/index.js");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//middleware for accessibility 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public')); 
+
+//HTML GET ROUTES
+
+// indexhtml GETroute 
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+//noteshtml GETroute
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
