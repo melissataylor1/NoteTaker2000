d49fbe4313f38f7d0086f8d0801feab606c80167
melissataylor1 <melissataylor@Melissas-MacBook-Pro.local>
2023-03-19 17:29:02 -0400

    troubleshooting uuid

--- server.js
+++ server.js
@@ -1,55 +1,60 @@
+
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-
 const app = express();
-const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
-app.use(express.urlencoded({extended: true}));
+const port = process.env.PORT || 3001;
+
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-// GET and POST request
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "/db/db.json"), (err,data) => {
-        if(err) throw err;
-        const notes = JSON.parse(data);
-        res.json(notes);
-    });
+app.get('/notes', (req, res) => {
+  res.sendFile(__dirname + '/public/notes.html');
+});
+
+function getDBContent() {
+  const dbStr = fs.readFileSync('./db/db.json', 'utf8');
+  return JSON.parse(dbStr);
+}
+
+app.get('/api/notes', (req, res) => {
+    const notes = getDBContent();
+    res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
-    const {title, text} = req.body;
-    if (!title || !text ) {
-        throw new Error("cant be blnk");
-    }
- const newNote = {title, text, id: uuidv4()};
- fs.readFile(path.join(__dirname, "/db/db.json"), (err,data) => {
-    if(err) {
-        console.log(err);
-    }
-    else {
-        const dbNotes = JSON.parse(data);
-        dbNotes.push(newNote);
-        fs.writeFile("./db/db.json", JSON.stringify(dbNotes),(err) => 
-        err ? console.error(err) : console.log("Successfully added note."))
-    }
-
- })
- const response = {
-    body: newNote
- }
- console.log(response)
- res.json(response)
-})
-
-
-
-// HTML routes for notes.html and index.html
-
-app.get("/notes", (req,res) => {
-    res.sendFile(path.join(__dirname,"/public/notes.html"));
+  console.log(req.body);
+  let notes = getDBContent() 
+    notes.push({
+      title: req.body.title,
+      text: req.body.text,
+      id: uuid(),
+    });
+    fs.writeFile('db/db.json', JSON.stringify(notes, null, 4), (err) => {
+      if (err) throw err;
+      res.json(notes);
+    })
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteID = req.params.id;
+
+   let notes = getDBContent()
+   
+    notes = notes.filter((note) => note.id !== noteID);
+
+    fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json(notes);
+    })
+});
+
+app.listen(port, () => console.log(`Server started at port ${port}`));
